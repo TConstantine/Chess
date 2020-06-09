6480d5a3a28a96c9d8bb0c0ee899b41f4fdecd98
Constantine Theodoridis <theodoridisconstantine@gmail.com>
2020-06-09 14:19:25 +0300

    MainMenuPresenter should display error message when board size is invalid

---
+++ app/src/main/java/constantine/theodoridis/android/game/chess/presentation/mainmenu/MainMenuPresenter.kt
@@ -0,0 +1,71 @@
+/*
+ *  Copyright (C) 2020 Constantine Theodoridis
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package constantine.theodoridis.android.game.chess.presentation.mainmenu
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import constantine.theodoridis.android.game.chess.domain.UseCase
+import constantine.theodoridis.android.game.chess.domain.request.MainMenuRequest
+import constantine.theodoridis.android.game.chess.domain.response.MainMenuResponse
+import constantine.theodoridis.android.game.chess.presentation.mainmenu.model.MainMenuViewModel
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.observers.DisposableSingleObserver
+import io.reactivex.schedulers.Schedulers
+
+class MainMenuPresenter(
+    private val useCase: UseCase<MainMenuRequest, MainMenuResponse>,
+    private val scheduler: Scheduler
+) : ViewModel() {
+    private val compositeDisposable = CompositeDisposable()
+    private val viewModelObservable = MutableLiveData<MainMenuViewModel>()
+
+    fun onStart(boardSize: Int, maxMoves: Int) {
+        compositeDisposable.add(Single.fromCallable { useCase.execute(createRequest(boardSize, maxMoves)) }
+            .subscribeOn(Schedulers.io())
+            .observeOn(scheduler)
+            .subscribeWith(object : DisposableSingleObserver<MainMenuResponse>() {
+                override fun onSuccess(response: MainMenuResponse) {
+                    viewModelObservable.postValue(createViewModel(response))
+                }
+
+                override fun onError(e: Throwable) {
+                }
+            })
+        )
+    }
+
+    fun viewModelObservable(): LiveData<MainMenuViewModel> {
+        return viewModelObservable
+    }
+
+    private fun createRequest(boardSize: Int, maxMoves: Int): MainMenuRequest {
+        return MainMenuRequest(
+            boardSize = boardSize,
+            maxMoves = maxMoves
+        )
+    }
+
+    private fun createViewModel(response: MainMenuResponse): MainMenuViewModel {
+        return MainMenuViewModel(
+            hasError = response.boardSizeErrorMessage != "",
+            boardSizeErrorMessage = response.boardSizeErrorMessage
+        )
+    }
+}
---
+++ app/src/test/java/constantine/theodoridis/android/game/chess/presentation/mainmenu/MainMenuPresenterTest.kt
@@ -0,0 +1,88 @@
+/*
+ *  Copyright (C) 2020 Constantine Theodoridis
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package constantine.theodoridis.android.game.chess.presentation.mainmenu
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.nhaarman.mockitokotlin2.any
+import constantine.theodoridis.android.game.chess.domain.UseCase
+import constantine.theodoridis.android.game.chess.domain.builder.MainMenuResponseBuilder
+import constantine.theodoridis.android.game.chess.domain.request.MainMenuRequest
+import constantine.theodoridis.android.game.chess.domain.response.MainMenuResponse
+import constantine.theodoridis.android.game.chess.presentation.mainmenu.model.MainMenuViewModel
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.Schedulers
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnit
+
+class MainMenuPresenterTest {
+    companion object {
+        private const val INVALID_BOARD_SIZE = 0
+        private const val MAX_MOVES = 3
+        private const val BOARD_SIZE_ERROR_MESSAGE = "Board size should be between 6 and 16"
+        private val MAIN_MENU_VIEW_MODEL = MainMenuViewModel(
+            hasError = true,
+            boardSizeErrorMessage = BOARD_SIZE_ERROR_MESSAGE
+        )
+    }
+
+    @Rule
+    @JvmField
+    val mockitoRule = MockitoJUnit.rule()!!
+
+    @Rule
+    @JvmField
+    val testExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var mockObserver: Observer<MainMenuViewModel>
+
+    @Mock
+    private lateinit var mockUseCase: UseCase<MainMenuRequest, MainMenuResponse>
+
+    private lateinit var presenter: MainMenuPresenter
+
+    @Before
+    fun setUp() {
+        presenter = MainMenuPresenter(mockUseCase, Schedulers.trampoline())
+        RxJavaPlugins.setIoSchedulerHandler { Schedulers.trampoline() }
+    }
+
+    @After
+    fun cleanUp() {
+        RxJavaPlugins.reset()
+    }
+
+    @Test
+    fun shouldDisplayErrorMessage_whenBoardSizeIsInvalid() {
+        presenter.viewModelObservable().observeForever(mockObserver)
+        val response = MainMenuResponseBuilder
+            .withBoardSizeErrorMessage(BOARD_SIZE_ERROR_MESSAGE)
+            .build()
+        `when`(mockUseCase.execute(any())).thenReturn(response)
+
+        presenter.onStart(INVALID_BOARD_SIZE, MAX_MOVES)
+
+        verify(mockObserver).onChanged(MAIN_MENU_VIEW_MODEL)
+    }
+}
